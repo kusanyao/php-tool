51e9273f64b13a2bb86958376cc1632e1ea08ffd
zhongshaoqing <shaoqingzhong@shein.com>
2018-12-21 19:06:30 +0800

    no message

--- class/McryptAesTool.calss.php
+++ class/McryptAesTool.calss.php
@@ -71,7 +71,7 @@ class McryptAesTool
     }
 }
 
-$aes = new AesTool();
+$aes = new McryptAesTool();
 $data = "abcdefghizklmnopqrstuvwxyz";
 $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
 
