9a51cf530bc0d99bd05639db546a4d94d10d5c94
zhongshaoqing <shaoqingzhong@shein.com>
2018-12-24 16:49:28 +0800

    no message

--- class/AesTool.class.php
+++ class/AesTool.class.php
@@ -19,7 +19,7 @@ class AesTool
             $iv    = openssl_random_pseudo_bytes($ivLen); // 生成相应长度的伪随机字节串作为初始化向量
         }
         $ciphertext = openssl_encrypt($plaintext, $method, $key, 0, $iv);
-        return $iv . $ciphertext;;
+        return $ciphertext;
     }
 
     /**
@@ -43,43 +43,43 @@ class AesTool
     }
 
     /**
-     * @param $data
-     * @param $key
-     * @param $method
-     * @param $mode
+     * @param        $data
+     * @param        $method
+     * @param        $mode
+     * @param        $key
+     * @param string $iv
      *
      * @return string
-     * @throws Exception
      */
-    public function mcryptEncrypt($data, $key, $method, $mode, $iv = '')
+    public function mcryptEncrypt($data, $method, $mode, $key, $iv = '')
     {
         if ($iv == '') {
-            $ivLen = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, $this->_mode);
+            $ivLen = mcrypt_get_iv_size($method, $mode);
             $iv    = mcrypt_create_iv($ivLen, MCRYPT_RAND);
         }
 
         $size       = mcrypt_get_block_size($method, $mode);
         $data       = $this->pkcs5Padding($data, $size);
+
         $ciphertext = mcrypt_encrypt($method, $key, $data, $mode, $iv);
-        $ciphertext = $iv . $ciphertext;
 
         return base64_encode($ciphertext);
     }
 
     /**
      * @param string $ciphertext
+     * @param string $method
+     * @param string $mode
      * @param string $key
-     * @param        $method
-     * @param        $mode
      * @param string $iv
      *
      * @return bool|string
      */
-    public function mcryptDecrypt(string $ciphertext, string $key, $method, $mode, string $iv = '')
+    public function mcryptDecrypt(string $ciphertext, string $method, string $mode, string $key, string $iv = '')
     {
         $ciphertext = base64_decode($ciphertext);
         if ($iv == '') {
-            $ivLen      = mcrypt_get_iv_size($method, $this->_mode);
+            $ivLen      = mcrypt_get_iv_size($method, $mode);
             $iv         = substr($ciphertext, 0, $ivLen);
             $ciphertext = substr($ciphertext, $ivLen);
         }
@@ -92,7 +92,7 @@ class AesTool
     /**
      * 填充
      */
-    private function pkcs5Padding($text, $blocksize)
+    private function pkcs5Padding(string $text, $blocksize)
     {
         $pad = $blocksize - (strlen($text) % $blocksize);
         return $text . str_repeat(chr($pad), $pad);
@@ -113,12 +113,38 @@ class AesTool
 }
 
 
-$originData = "独孤求败是香港武侠小说家金庸的作品里一位武林高手，是小说中唯一被提及过真正“无敌于天下”的高手，于小说中从未出场过，而其事迹只在极少数武林人士之间口耳相传。其名字曾于金庸的三部小说中出现，分别为《神雕侠侣》、《笑傲江湖》以及《鹿鼎记》。《神雕侠侣》主角杨过习得独孤求败使用重剑以及其修练内力的法门后，继以晋身当代绝顶高手之列。《笑傲江湖》主角令狐冲原本武功平平，因缘际会学得独孤九剑以后，一跃为当代剑术高手。《鹿鼎记》只有一句提及独孤求败，是澄观和尚想及“无招胜有招”的前人例子时念起。";
+$originData = "独孤求败是香港武侠小说家金庸的作品里一位武林高手，是小说中唯一被提及过真正“无敌于天下”的高手，于小说中从未出场过，
+而其事迹只在极少数武林人士之间口耳相传。其名字曾于金庸的三部小说中出现，分别为《神雕侠侣》、《笑傲江湖》以及《鹿鼎记》。
+《神雕侠侣》主角杨过习得独孤求败使用重剑以及其修练内力的法门后，继以晋身当代绝顶高手之列。
+《笑傲江湖》主角令狐冲原本武功平平，因缘际会学得独孤九剑以后，一跃为当代剑术高手。
+《鹿鼎记》只有一句提及独孤求败，是澄观和尚想及“无招胜有招”的前人例子时念起。";
+echo "data:  " . $originData . PHP_EOL;
+echo 'DataLen:  ' . strlen($originData) . PHP_EOL;
+
+echo '-------------------------' . PHP_EOL;
 
 $aesTool = new AesTool();
 
-$opensslCiphertext = $aesTool->opensslEncrypt($originData, 'aes-128-cbc', '1246');
-echo '$opensslCiphertext:  ' . $opensslCiphertext . PHP_EOL;
+$ciphertext = $aesTool->opensslEncrypt($originData, 'aes-128-cbc', 'aaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaa');
+echo '$ciphertext:  ' . $ciphertext . PHP_EOL;
+echo 'DataLen:  ' . strlen($ciphertext) . PHP_EOL;
 
-$opensslPlaintext = $aesTool->opensslDecrypt($opensslCiphertext, 'aes-128-cbc', '1246');
-echo '$opensslCiphertext:  ' . $opensslCiphertext . PHP_EOL;
+
+
+$plaintext = $aesTool->opensslDecrypt($ciphertext, 'aes-128-cbc', 'aaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaa');
+echo '$plaintext:  ' . $plaintext . PHP_EOL;
+echo 'DataLen:  ' . strlen($plaintext) . PHP_EOL;
+
+
+echo '-------------------------' . PHP_EOL;
+
+
+$ciphertext = $aesTool->mcryptEncrypt($originData, MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC, 'aaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaa');
+echo '$ciphertext:  ' . $ciphertext . PHP_EOL;
+echo 'DataLen:  ' . strlen($ciphertext) . PHP_EOL;
+
+
+
+$plaintext = $aesTool->mcryptDecrypt($ciphertext, MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC, 'aaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaa');
+echo '$plaintext:  ' . $plaintext . PHP_EOL;
+echo 'DataLen:  ' . strlen($plaintext) . PHP_EOL;
